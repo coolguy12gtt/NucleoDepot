d167b6e6a0097378952021804d36da16231d20ea
coolguy12gtt <aaronslamovich@gmail.com>
2022-03-20 14:01:19 -0400

    trying to whitelist again

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.hosts = nil
+  config.host_authorization = { exclude: ->(request) { request.path =~ /healthcheck/ } }
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
