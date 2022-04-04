4abed3fac6683aa18e7c8c75aebb7a7273c505b1
coolguy12gtt <aaronslamovich@gmail.com>
2022-04-04 14:49:17 -0900

    tracking ip_addr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,13 @@ class ApplicationController < ActionController::Base
   before_action :ipTracer
 
   def ipTracer  
-    ip_addr = "
+    ip_addr = ""
     time = ""
-    ip_Addr = request.remote_ip
-    time = Time.now.getetc
+    ip_addr = request.remote_ip
+    time = Time.now.getutc
 
-    File.write("ipTracer.txt", "#{ip_addr} - @ #{time}\n")
+    File.open("ipTracer.txt", 'a') do |f|
+      f << "#{ip_addr} - @ #{time}\n"
+    end
   end
 end
